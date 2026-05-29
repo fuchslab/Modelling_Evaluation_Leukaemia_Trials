3599dff33f6d47bfef780f4bad8eba6ee6754ab4
jwaesche21 <94894872+jwaesche21@users.noreply.github.com>
2026-05-29 11:26:57 +0200

    Update 01_identifiability_analysis.R

--- Simulation_study/01_identifiability_analysis.R
+++ Simulation_study/01_identifiability_analysis.R
@@ -18,107 +18,108 @@ library("latex2exp")
 ##### Generate exponential growth model for dMod #####
 # ODE model
 ode_equation <- eqnvec(x1 = "beta1*x1", x2 = "beta2*x2")
-model_dMod <- odemodel(ode_equation, modelname = "exponential_model")
-x_dMod <- Xs(model_dMod)
+model <- odemodel(ode_equation, modelname = "exponential_model") 
+x_dMod <- Xs(model)
 
-# Observable model (for both estimation and simulation)
-observable <- eqnvec(y = "log(x1/(x1+x2))-(sigma^2)/2")
+# Observable model
+observable <- eqnvec(y = "x1/(x1+x2)")
 g_dMod <- Y(observable, x_dMod, compile = T, attach.input = F)
-observable_sim <- eqnvec(y = "x1/(x1+x2)")
-g_sim <- Y(observable_sim, x_dMod, compile = T, attach.input = F)
 
 # Error model
 error_pars <- eqnvec(y = "sigma")
-error_dMod <- Y(
-  error_pars, f = c(observable, ode_equation), attach.input = TRUE, compile = TRUE)
+error_dMod <- Y(error_pars, f = c(observable, ode_equation), attach.input = T, compile = T)
 
-# Parameter transformations for original full model 
-p_full <- P(trafo=eqnvec(
-  x1 = "exp(x1)", x2 = "exp(x2)", beta1 = "beta1", beta2 = "beta2", 
-  sigma = "exp(sigma)"), condition = "C1")
-outerpars_full <- getParameters(p_full)
+# Parameter transformations
+p_full <- P(eqnvec(
+  beta1 = "beta1", beta2 = "beta2", x1 = "exp(x1)", x2 = "exp(x2)", 
+  sigma = "exp(sigma)"), condition = "standard") 
 
 # Reparametrisations to make model structurally identifiable
 equations <- getEquations(p_full)
-equations[["C1"]]["beta2"] <- "theta1+beta1"
-equations[["C1"]]["x2"] <- "exp(theta2)*exp(x1)"
-p_repar <- P(equations[["C1"]], condition = "C1")
-outerpars <- getParameters(p_repar)
-theta <- c("theta1", "theta2", "sigma")
+equations[["standard"]]["beta2"] <- "theta1+beta1"
+equations[["standard"]]["x2"] <- "exp(theta2)*exp(x1)"
+p_dMod <- P(equations[["standard"]], condition = "standard")
 
 
 ##### Simulate data for structural identifiability analysis #####
-set.seed(90)
+set.seed(300)
 
 # Parameter values
-pars <- c(x1 = 10, x2 = 10, beta1 = 0.1, beta2 = 0.2)
-sigma <- 0.2
+pars <- c(x1 = 50, x2 = 50, beta1 = 0.3, beta2 = 0.5)
+sigma <- 0.05
 
 # Time range
 times <- seq(0, 50, 1)
 
-# Simulation with dMod 
-out <- (g_sim*x_dMod)(times, pars)
-data_sim <- do.call(rbind, replicate(20, out[[1]], simplify = FALSE))
-data_sim <- data_sim[order(data_sim[,1]),]
+# Simulation
+out <- (g_dMod*x_dMod)(times, pars)
 
-# Multiplying simulated observable with log-normally distributed measurement error
-data_sim[,2] <- data_sim[,2]*rlnorm(nrow(data_sim), (-(sigma^2)/2), sigma)
-data <- data.frame(name = rep("y", nrow(data_sim)), time = data_sim[,1], 
-                   value = log(data_sim[,2]), sigma = rep(NA,nrow(data_sim)))
-data <- datalist(C1 = data)
+data_sim <- do.call(rbind, replicate(20, out[[1]], simplify = FALSE))
+data_sim <- data_sim[order(data_sim[, 1]),]
+
+# Add normally distributed error to simulated observable
+data_sim[, 2] <- data_sim[,2] + rnorm(nrow(data_sim), mean = 0, sd = sigma)
+data <- data.frame(
+  name = rep("y", nrow(data_sim)), time = data_sim[, 1], value = data_sim[,2], 
+  sigma = rep(NA, nrow(data_sim)))
+data_dMod <- datalist(standard = data)
 
 
 ##### Identifiability of full model #####
 ### Parameter estimation
-set.seed(99)
+set.seed(33)
 
 # Center of starting values for parameter estimation
-pouter <- structure(
-  c(log(pars["x1"]), log(pars["x2"]), pars["beta1"], pars["beta2"],
-    log(sigma)), names = outerpars_full)
+pouter <- structure(c(
+  pars["beta1"], pars["beta2"], log(pars["x1"]), log(pars["x2"]), log(sigma)),
+  names = c("beta1", "beta2", "x1", "x2", "sigma"))
 
 # Objective function
-obj <- normL2(
-  data, g_dMod*x_dMod*p_full, errmodel = error_dMod) + constraintL2(pouter, sigma = 5)
+obj <- normL2(data_dMod, g_dMod*x_dMod*p_full, errmodel = error_dMod) + constraintL2(
+  pouter, sigma = 3)
 
 # Multiple parameter estimations with randomly chosen starting values
-fits <- mstrust(obj, pouter, studyname = "multiple_fits", rinit = 0.1, rmax = 10, 
-                cores = 4, fits = 50, samplefun = "rnorm", iterlim = 500)
+fits <- mstrust(
+  obj, pouter, studyname = "multiple_fits", rinit = 0.1, rmax = 10, cores = 4, 
+  fits = 50, samplefun = "rnorm", iterlim = 500)
 summary(fits)
 
 # Extract estimated parameters of best estimation run
 bestfit <- as.parvec(as.parframe(fits))
 
 ### Profile likelihoods
-profiles <- profile(obj = obj, pars = bestfit, whichPar = names(bestfit), 
-                    alpha = 0.05, cores = 4)
+profiles <- profile(
+  obj = obj, pars = bestfit, whichPar = names(bestfit), alpha = 0.05, cores = 5)
 plotProfile(profiles, mode == "data")
 
 
 ##### Identifiability of reparametrised model #####
 ### Parameter estimation
-set.seed(30)
+set.seed(98)
 
 # Center of starting values for parameter estimation
-pouter <- structure(c(log(pars["x1"]), log(pars["x2"]/pars["x1"]), pars["beta1"],
-                      pars["beta2"]-pars["beta1"], log(sigma)), names = outerpars)
+pouter <- structure(
+  c(pars["beta2"]-pars["beta1"], pars["beta1"], log(pars["x2"]/pars["x1"]), 
+    log(pars["x1"]), log(sigma)),
+  names = c("theta1", "beta1", "theta2", "x1", "sigma"))
 
 # Objective function
-obj <- normL2(
-  data, g_dMod*x_dMod*p_repar, errmodel = error_dMod) + constraintL2(pouter, sigma = 3)
+obj <- normL2(data_dMod, g_dMod*x_dMod*p_dMod, errmodel = error_dMod) + constraintL2(
+  pouter, sigma = 3)
 
 # Multiple parameter estimations with randomly chosen starting values
-fits <- mstrust(obj, pouter, studyname = "multiple_fits", rinit = 0.1, rmax = 10, 
-                cores = 4, fits = 50, samplefun = "runif", iterlim = 500)
+fits <- mstrust(
+  obj, pouter, studyname = "multiple_fits", rinit = 0.1, rmax = 10, cores = 5,
+  fits = 50, samplefun = "runif", iterlim = 500)
 summary(fits)
 
 # Extract estimated parameters of best estimation run
 bestfit <- as.parvec(as.parframe(fits))
 
 ### Profile likelihoods
-profiles <- profile(obj = obj, pars = bestfit, whichPar = theta, alpha = 0.05,
-                    cores = 4)
+profiles <- profile(
+  obj = obj, pars = bestfit, whichPar = c("theta1", "theta2", "sigma"), 
+  alpha = 0.05, cores = 3)
 plotProfile(profiles, mode == "data")
 
 
@@ -130,65 +131,68 @@ plotProfile(profiles, mode == "data")
 # ODE model
 ode_equation <- eqnvec(
   x1 = "lambda1*x1*(1-(x1+x2)/K)", x2 = "lambda2*x2*(1-(x1+x2)/K)")
-model_dMod <- odemodel(ode_equation, modelname = "logistic_model") 
-x_dMod <- Xs(model_dMod) 
+model <- odemodel(ode_equation, modelname = "logistic_model") 
+x_dMod <- Xs(model) 
 
-# Observable (for both estimation and simulation)
-observable <- eqnvec(y = "log(x1/(x1+x2))-(sigma^2)/2")
+# Observable
+observable <- eqnvec(y = "x1/(x1+x2)")
 g_dMod <- Y(observable, x_dMod, compile = T, attach.input = F)
-observable_sim <- eqnvec(y = "x1/(x1+x2)")
-g_sim <- Y(observable_sim, x_dMod, compile = T, attach.input = F)
 
 # Error model
 error_pars <- eqnvec(y = "sigma")
-error_dMod <- Y(
-  error_pars, f = c(observable, ode_equation), attach.input = TRUE,  compile = TRUE)
+error_dMod <- Y(error_pars, f = c(observable, ode_equation), attach.input = T, compile = T)
 
 # Parameter transformations
-p_dMod <- P(trafo=eqnvec(
-  lambda1 = "lambda1", lambda2 = "lambda2", K = "exp(K)", x1 = "exp(x1)", 
-  x2 = "exp(x2)", sigma = "exp(sigma)"), condition = "C1")
-outerpars <- getParameters(p_dMod)
+p_dMod <- P(
+  eqnvec(
+    lambda1 = "lambda1", lambda2 = "lambda2", K = "exp(K)", x1 = "exp(x1)", 
+    x2 = "exp(x2)", sigma = "exp(sigma)"), condition = "standard")
 
 
 ##### Simulate data for structural identifiability analysis #####
 set.seed(59)
 
 # Parameter values
-pars <- c(x1 = 10, x2 = 10, lambda1 = 0.1, lambda2 = 0.2, K = 1e3) 
-sigma <- 0.2
+pars <- c(x1 = 50, x2 = 50, lambda1 = 0.1, lambda2 = 0.3, K = 1e3)
+sigma <- 0.05
 
 # Time range
 times <- seq(0, 50, 1)
 
 # Simulation with dMod 
-out <- (g_sim*x_dMod)(times, pars)
-data_sim <- do.call(rbind, replicate(20, out[[1]], simplify = FALSE))
+out <- (g_dMod*x_dMod)(times, pars)
+data_sim <- do.call(rbind, replicate(20, out[[1]], simplify = F))
 data_sim <- data_sim[order(data_sim[,1]),]
 
-# Multiplying simulated observable with log-normally distributed measurement error
-data_sim[,2] <- data_sim[,2]*rlnorm(nrow(data_sim), (-(sigma^2)/2), sigma)
-data <- data.frame(name = rep("y", nrow(data_sim)), time = data_sim[,1], 
-                   value = log(data_sim[,2]), sigma = rep(NA,nrow(data_sim)))
-data <- datalist(C1 = data)
+# Add normally distributed error to simulated observable
+data_sim[,2] <- data_sim[,2] + rnorm(nrow(data_sim), mean = 0, sigma)
+data <- data.frame(name = rep("y",nrow(data_sim)), time = data_sim[,1], 
+                   value = data_sim[,2], sigma = rep(NA,nrow(data_sim)))
+data_dMod <- datalist(standard = data)
 
 
 ##### Identifiability of full model #####
 ### Parameter estimation
 set.seed(100)
 
+
+ #sigma=10
+
+#####
+
 # Center of starting values for parameter estimation
 pouter <- structure(
   c(pars["lambda1"], pars["lambda2"], log(pars["K"]), log(pars["x1"]), 
-    log(pars["x2"]), log(sigma)), names = outerpars)
+    log(pars["x2"]), log(sigma)), 
+  names = c("lambda1", "lambda2", "K", "x1", "x2", "sigma"))
 
 # Objective function
-obj <- normL2(
-  data, g_dMod*x_dMod*p_dMod, errmodel = error_dMod) + constraintL2(pouter, sigma = 10)
+obj <- normL2(data_dMod, g_dMod*x_dMod*p_dMod, errmodel = error_dMod) + constraintL2(
+  pouter, sigma=10)
 
 # Multiple parameter estimations with randomly chosen starting values
 fits <- mstrust(obj, pouter, studyname = "multiple_fits", rinit = 0.1, rmax = 10, 
-                cores = 4, fits = 50, samplefun = "rnorm", iterlim = 500)
+                cores = 5, fits = 50, samplefun = "rnorm", iterlim = 500)
 summary(fits)
 
 # Extract estimated parameters of best estimation run 
@@ -196,7 +200,7 @@ bestfit <- as.parvec(as.parframe(fits))
 
 ### Profile likelihoods
 profiles <- profile(obj = obj, pars = bestfit, whichPar = names(bestfit), 
-                    alpha = 0.05, cores = 4)
+                    alpha = 0.05, cores = 6)
 plotProfile(profiles, mode == "data")
 
 
@@ -204,22 +208,29 @@ plotProfile(profiles, mode == "data")
 ### Parameter estimation
 set.seed(100)
 
+
+ #sigma=10
+
+#####
+
 # Specify fixed parameter x1
 fixed_par <- c("x1") 
 
 # Center of starting values for parameter estimation
 pouter <- structure(
   c(pars["lambda1"], pars["lambda2"], log(pars["K"]), log(pars["x1"]), 
-    log(pars["x2"]), log(sigma)), names = outerpars)
+    log(pars["x2"]), log(sigma)), 
+  names = c("lambda1", "lambda2", "K", "x1", "x2", "sigma"))
 
 # Objective function
-obj <- normL2(
-  data, g_dMod*x_dMod*p_dMod, errmodel = error_dMod) + constraintL2(pouter, sigma = 10) 
+obj <- normL2(data_dMod, g_dMod*x_dMod*p_dMod, errmodel = error_dMod) + constraintL2(
+  pouter, sigma=10) 
 
 # Multiple parameter estimations with randomly chosen starting values
-fits <- mstrust(obj, pouter[!names(pouter) %in% fixed_par], fixed = pouter[fixed_par],  
-                studyname = "multiple_fits", rinit = 0.1, rmax = 10, cores = 4,
-                fits = 50, samplefun = "rnorm", iterlim = 500)
+fits <- mstrust(
+  obj, pouter[!names(pouter) %in% fixed_par], fixed = pouter[fixed_par],  
+  studyname = "multiple_fits", rinit = 0.1, rmax = 10, cores = 5, fits = 50, 
+  samplefun = "rnorm", iterlim = 500)
 summary(fits)
 
 # Extract estimated parameters of best estimation run 
@@ -227,6 +238,6 @@ bestfit <- as.parvec(as.parframe(fits))
 
 ### Profile likelihoods
 profiles <- profile(obj = obj, pars = bestfit, fixed = pouter[fixed_par],
-                    whichPar = names(bestfit), alpha = 0.05, cores = 4)
+                    whichPar = names(bestfit), alpha = 0.05, cores = 5)
 plotProfile(profiles, mode == "data")
 
